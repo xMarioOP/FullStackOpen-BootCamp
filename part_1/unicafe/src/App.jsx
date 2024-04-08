d526ed9c7fc670397905ac8bcb9824c97d61e0d7
xMarioOP <marioalejandro424@outlook.com>
2024-04-07 20:09:59 -0500

    Exercise 1.8

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,9 +1,24 @@
 import { useState } from 'react'
 
+const Statistics =  ({good, neutral, bad, all, average, positive}) => {
+  console.log(good, neutral, bad, all, average, positive);
+  return(
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all} </p>
+      <p>average {average}</p> 
+      <p>positive {positive} % </p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [bad, setBad] = useState(0) 
 
   const handleGoodClick = (newValue) => () => {
     console.log("value good before", newValue);
@@ -35,13 +50,7 @@ const App = () => {
       <button onClick={handleGoodClick(good +1 )}>good</button>
       <button onClick={handleNeutralClick(neutral +1 )}>neutral</button>
       <button onClick={handleBadClick(bad +1 )}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all} </p>
-      <p>average {average}</p> 
-      <p>positive {positive} % </p>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive}  />
     </div>
   )
 } 
