7922ff1897a24139c96179fdbba071b9e9cd710b
xMarioOP <marioalejandro424@outlook.com>
2024-04-16 20:00:44 -0500

    Exercise 2.4

--- part_2/courseinfo/src/components/Content.jsx
+++ part_2/courseinfo/src/components/Content.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import Parts from './Parts';
 
 const Content = ({parts}) => {
-    const totalExercises = parts.reduce((total, part) => {
-        console.log("QUE PASA", total, part)
-       return total + part.exercises;
-    }, 0)
-
+    const totalExercises = parts.reduce((total, parts) => total + parts.exercises, 0)
       return (
         <div>
             { 
