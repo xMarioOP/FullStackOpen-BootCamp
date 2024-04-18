d359187124b93a646ae0e0d961c05a7a9d190a45
xMarioOP <marioalejandro424@outlook.com>
2024-04-17 19:19:18 -0500

    Exercise 2.7

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -3,12 +3,18 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
 
-  const addPerson = (e) =>{
+  const addPerson = (e) => {
     e.preventDefault()
     console.log(e);
+
+    const nameExist = persons.some(person => person.name === newName)
+    if (nameExist) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
 
     const addPersonObject = {
       name: newName
@@ -19,19 +25,19 @@ const App = () => {
   }
 
 
-   const handleChange = (e) =>{
+  const handleChange = (e) => {
     console.log(e.target.value);
     setNewName(e.target.value)
   }
- 
+
   return (
     <div>
       <h2>Phonebook</h2>
 
-      <form onSubmit={addPerson}> 
+      <form onSubmit={addPerson}>
         <div>
-          name: 
-          <input 
+          name:
+          <input
             value={newName}
             onChange={handleChange}
           />
@@ -43,11 +49,11 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-        {
-          persons .map(person => 
-             <p key={person.name}>{person.name}</p>
-          )
-        }
+      {
+        persons.map(person =>
+          <p key={person.name}>{person.name}</p>
+        )
+      }
     </div>
   )
 }
